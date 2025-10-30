a4b5a0929c33fcceec0b6c1b884ffc1a74d192ff
antoniorv6 <ariosvila@gmail.com>
2025-10-30 14:02:35 +0100

    Broken bird with cmake working

---
+++ build/src/CMakeFiles/flappybirddca.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/antoniorv6/Dev/broken-bird-dca/src/GameOverState.cpp" "src/CMakeFiles/flappybirddca.dir/GameOverState.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/GameOverState.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/GameState.cpp" "src/CMakeFiles/flappybirddca.dir/GameState.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/GameState.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/MainGameState.cpp" "src/CMakeFiles/flappybirddca.dir/MainGameState.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/MainGameState.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/StateMachine.cpp" "src/CMakeFiles/flappybirddca.dir/StateMachine.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/StateMachine.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/main.cpp" "src/CMakeFiles/flappybirddca.dir/main.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/main.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/systems/InputSystem.cpp" "src/CMakeFiles/flappybirddca.dir/systems/InputSystem.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/systems/InputSystem.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/systems/PhysicsSystem.cpp" "src/CMakeFiles/flappybirddca.dir/systems/PhysicsSystem.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/systems/PhysicsSystem.cpp.o.d"
+  "/home/antoniorv6/Dev/broken-bird-dca/src/systems/RenderSystem.cpp" "src/CMakeFiles/flappybirddca.dir/systems/RenderSystem.cpp.o" "gcc" "src/CMakeFiles/flappybirddca.dir/systems/RenderSystem.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/CMakeFiles/flappybirddca.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flappybirddca.dir/GameOverState.cpp.o"
+  "CMakeFiles/flappybirddca.dir/GameOverState.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/GameState.cpp.o"
+  "CMakeFiles/flappybirddca.dir/GameState.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/MainGameState.cpp.o"
+  "CMakeFiles/flappybirddca.dir/MainGameState.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/StateMachine.cpp.o"
+  "CMakeFiles/flappybirddca.dir/StateMachine.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/main.cpp.o"
+  "CMakeFiles/flappybirddca.dir/main.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/systems/InputSystem.cpp.o"
+  "CMakeFiles/flappybirddca.dir/systems/InputSystem.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/systems/PhysicsSystem.cpp.o"
+  "CMakeFiles/flappybirddca.dir/systems/PhysicsSystem.cpp.o.d"
+  "CMakeFiles/flappybirddca.dir/systems/RenderSystem.cpp.o"
+  "CMakeFiles/flappybirddca.dir/systems/RenderSystem.cpp.o.d"
+  "flappybirddca"
+  "flappybirddca.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flappybirddca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
